5cb2e0737c2ad7799717e3cceb53ebce41e7b1aa
kozlov_vd <kozlov_vd@magnit.ru>
2026-08-23 16:10:39 +0300

    feat: изменения с урока

--- api.py
+++ api.py
@@ -3,7 +3,10 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-    
+@app.get('/')
+async def home():
+    return {'msg': 'hello world'}
+
 movies = [
     {
         'id': 1,
@@ -22,27 +25,11 @@ movies = [
     },
 ]
 
-movie_id_max = 3
 
-
-
-@app.get('/')
-async def home():
-    return {'msg': 'hello world'}
-
-
-@app.get('/movie/{movie_id}')
-async def get_movie_by_id(movie_id: int):
-    found = [movie for movie in movies if movie['id']==movie_id]
-    if len(found) == 0:
-        raise HTTPException(status_code=404, detail='Не найден фильм по id')
-    if len(found) > 1:
-        raise ValueError('Найдено несколько фильмов по одному id, они не уникальны')
-    return found[0]
-    
-
-@app.get('/movie')
-async def get_movies_all(offset: int = 0, limit: int | None = None, views: int | None = None):
+@app.get('/movie', status_code=200, summary='Возвращает фильмы')
+async def get_movies_all(
+    offset: int = 0, limit: int | None = None, views: int | None = None
+    ):
     movies_selected = movies[offset:]
     if limit:
         movies_selected = movies_selected[:limit]
@@ -53,9 +40,25 @@ async def get_movies_all(offset: int = 0, limit: int | None = None, views: int |
     return movies_selected
 
 
+
+@app.get('/movie/{movie_id}')
+async def get_movie_by_id(movie_id: int):
+    movie_list = [movie for movie in movies if movie['id'] == movie_id]
+    
+    if len(movie_list) == 0:
+        raise HTTPException(status_code=404, detail=f'Фильм с id={movie_id} не найден')
+
+    if len(movie_list) > 1:
+        raise ValueError(f'Для id={movie_id} для фильма')
+    
+    return movie_list[0]
+
+
 class MovieData(BaseModel):
     title: str
     views: int
+    
+movie_id_max = 3
 
 
 @app.post('/movie')
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import uvicorn
-from api import app
 
 if __name__ == '__main__':
     uvicorn.run(
         app='api:app',
         host='0.0.0.0',
-        port=8100,
+        port=8200,
         reload=True,
         timeout_graceful_shutdown=0,
         log_level='debug'
